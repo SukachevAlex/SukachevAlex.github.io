6cd367cf1cfa97fd92c9656b4577c963079e1adb
SukachevAlex <sukachexalex@gmail.com>
2018-03-30 17:01:14 +0300

    add missing pages

--- Hemma/js/main.js
+++ Hemma/js/main.js
@@ -25,21 +25,58 @@ $(document).ready(function() {
         ]
     });
 
+
     ymaps.ready(function () {
-        var myMap = new ymaps.Map('map', {
-                center: [55.751574, 37.573856],
-                zoom: 7,
-                type: 'yandex#hybrid',
-                controls: []
-            }, {
-                suppressMapOpenBlock: true
-            }),
+        if ($("#map").length) {
+            var myMap = new ymaps.Map('map', {
+                    center: [55.751574, 37.573856],
+                    zoom: 7,
+                    type: 'yandex#hybrid',
+                    controls: []
+                }, {
+                    suppressMapOpenBlock: true
+                }),
 
-            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {})
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {});
 
-        myMap.geoObjects
-            .add(myPlacemark);
+            myMap.geoObjects
+                .add(myPlacemark);
+        }
+        if ($("#map_scotland").length) {
+            var myMap = new ymaps.Map('map_scotland', {
+                    center: [56.879663, -4.589376],
+                    zoom: 9,
+                    type: 'yandex#hybrid',
+                    controls: []
+                }, {
+                    suppressMapOpenBlock: true
+                }),
+
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {});
+
+            myMap.geoObjects
+                .add(myPlacemark);
+        }
+
+        if ($("#map_norway").length) {
+            var myMap = new ymaps.Map('map_norway', {
+                    center: [64.570968, 11.966377],
+                    zoom: 9,
+                    type: 'yandex#hybrid',
+                    controls: []
+                }, {
+                    suppressMapOpenBlock: true
+                }),
+
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {});
+
+            myMap.geoObjects
+                .add(myPlacemark);
+        }
+
+
     });
+
 
     $(document).on('click', 'a[href^="#"]', function (event) {
         event.preventDefault();
@@ -48,4 +85,18 @@ $(document).ready(function() {
         }, 500);
     });
 
+});
+
+
+$(window).on('load', function() {
+    var counter = 0;
+    var i = setInterval(function(){
+        $('.percentage').html(counter + '%');
+        $('.progress-bar').css('width', counter + '%');
+        counter++;
+        if(counter === 101) {
+            clearInterval(i);
+            $('.preloader__wrapper').fadeOut('slow');
+        }
+    }, 10);
 });
